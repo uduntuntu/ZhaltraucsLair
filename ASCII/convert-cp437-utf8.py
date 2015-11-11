9803b27ed976d5c81c4ccdbdf32398d959bd07d8
Ari-Martti Hopiavuori <arimarth@metropolia.fi>
2015-11-11 04:15:19 +0200

    made basic code snippet to function and use it.

--- ASCII/convert-cp437-utf8.py
+++ ASCII/convert-cp437-utf8.py
@@ -1,7 +1,11 @@
-f = open('otsikko_test.asc',encoding='cp437',mode='r')
-ascii = f.read()
-f.close()
+def convertCP437toUTF8(inputFile, outputFile)
+    f = open(inputFile,encoding='cp437',mode='r')
+    original = f.read()
+    f.close()
 
-f = open('otsikko_unicode.asc', encoding='UTF-8', mode='w')
-f.write(ascii)
-f.close()
+    f = open(outputFile, encoding='UTF-8', mode='w')
+    f.write(original)
+    f.close()
+    return
+
+convertCP437toUTF8('Otsikko_test.asc','otsikko_unicode.asc')
