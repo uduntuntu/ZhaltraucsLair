cc7340db5403532043fc61c0b794d15bcc053adf
Ari-Martti Hopiavuori <arimarth@metropolia.fi>
2015-11-19 08:31:39 +0200

    refactored database-related code to own file in functions directory.

--- setup.py
+++ setup.py
@@ -1,22 +1,3 @@
 # -*- coding: utf-8 -*-
-import yaml
-import mysql.connector
-from mysql.connector import errorcode
-
-with open("config.yaml", 'r') as ymlfile:
-    cfg = yaml.load(ymlfile)
-
-try:
-  cnx = mysql.connector.connect(user=cfg['MariaDB']['user'],
-                              password=cfg['MariaDB']['passwd'],
-                              host=cfg['MariaDB']['host'],
-                              database=cfg['MariaDB']['db'])
-except mysql.connector.Error as err:
-  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-    print("Something is wrong with your user name or password")
-  elif err.errno == errorcode.ER_BAD_DB_ERROR:
-    print("Database does not exist")
-  else:
-    print(err)
-else:
-    cnx.close()
+import functions.database as db
+db.testConnection()
