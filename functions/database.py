3a55d6c760b99e9495c9e1549c812da423b22bb4
kerkko rasilainen <kekenin@gmail.com>
2015-11-24 14:20:34 +0200

    added two more Orcs to zl_NPC table with descriptions

--- functions/database.py
+++ functions/database.py
@@ -272,7 +272,9 @@ def createNPC():
     sql = "INSERT into ZL_NPC" \
           "(`Name`,`ID`,`HP`,`Agility`,`Strength`,`RoomID`,`PointModifier`,`Description`) " \
           "values ('A Rat', 1, 10, 5, 2, 1, 5, null)," \
-          "('An Orc', 2, 15, 2, 5, null, 10, null)"
+          "('An Orc', 2, 15, 2, 5, null, 10, 'A green, snout nosed, nasty looking Orc.')," \
+          "('An Orc', 3, 15, 2, 5, null, 10, 'A green, snout nosed, nasty looking Orc.')," \
+          "('An Orc', 3, 15, 2, 5, null, 10, 'A green, snout nosed, nasty looking Orc.')"
     cur.execute(sql)
     cur.close()
     cnx.commit()
