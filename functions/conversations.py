cd82111be6e690c753fed3d0326b270aefac2482
MAXIMIES <spoljaricmax@gmail.com>
2015-12-03 12:38:07 +0200

    added while loops to covnersations

--- functions/conversations.py
+++ functions/conversations.py
@@ -84,6 +84,12 @@ def talk(npc):
         print("Y/N")
     
         choice=input()
+        
+        while answer not in (y,n,Y,N):
+                try:
+                    answer=int(input("What to do? "))
+                except ValueError as e:
+                    print(e)
         if choice=="y" or choice=="Y":
             print("Right as the rat is going for the goblins juggular, you kick the foul beast in the ribs, sending it ")
             print("flying accross the room accompanied with a high-pitched squeal. The goblin runs for cover and the rat ")
@@ -105,3 +111,40 @@ def talk(npc):
             print("You carefully skip around the gurgling goblin as the rat tears at it's throat. You clearly are of low ")
             print("empathy and even though you pretend to not care, the hopeless and fear-ridden screams of a dying humanoid")
             print("will haunt you for the rest of your life.")
+# coding=cp1252
+#room 17 scene
+
+    if npc.ID==8:
+        print("Do you wish to free the slave?")
+        print()
+        print("Y/N")
+        choice8=input()
+        while answer not in (y,n,Y,N):
+                try:
+                    answer=int(input("What to do? "))
+                except ValueError as e:
+                    print(e)
+    
+        if choice8=="y" or choice8=="Y":
+            print("You Attack the orc's! Battle commences!")
+    
+            combat()
+    
+            if npc.hp<=0:
+                print("""The prisoner approaches you, relief visible in her face. "I tought i'd never see another day , thank""")
+                print("""you stranger. Like you, i was on my way to destroy Zhaltrauc. I just couldn't stand watching the """)
+                print("""realm suffer as he started to grow in power. But apparently i was not as prepared as i tougth. I do,""")
+                print("""however, have a valuable piece of information. Zhaltrauc himself is an old evil spirit of aking """)
+                print("""thrown down from his throne by the people he reigned over. He assumed the form of a lich king and""")
+                print("""stole the sacred Light Stone from my father's, the king of my land, castle. It's what he uses to """)
+                print("""channel the evil powers and wouldn't have lasted this long without it. The stone is used for good """)
+                print("""but in the wrong hands... but i digress. Beware of the stone! He keeps it on his crown and will""")
+                print("""youse it against you! You will go blind and be unable to fight. You need to find a way to fight""")
+                print("""the blinding light that the magical gem emanates. I wish for you to exceed. Farewell!" The """)
+                print("""adventuring maiden gives you a health potion. She then takes a sword from the orc guards and makes""")
+                print("""her way back to the first level. """)
+    
+        elif choice8=="n" or choice8=="N":
+            print("You hide behind a pillar and observe as the prisoner is dragged away.")
+            print("You can only guess what will come of her.")
+
